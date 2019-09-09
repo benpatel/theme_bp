7a1452e377c4b6bf51a53f7454f6dbac1f48a3b4
Ben <binal0072003@gmail.com>
2019-09-08 23:42:32 -0400

    upate

---
+++ page-portfolio.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<div class="main-section page-section">
+<?php
+	if(have_posts()):
+
+		while (have_posts()): the_post();
+		?>
+
+  <div class="page-title">
+    <h2><?php wp_title(''); ?></h2>
+  </div>
+  <div class="page-content col-12">
+    <div id="portfolio-page" class="row m-0">
+        <div class="col-lg-12">
+          <div  class="portfolio" >
+            
+               <?php the_content(); ?>
+          </div>
+        </div>
+
+    </div>
+
+  </div>
+  <div class="page-footer"></div>
+  <?php
+endwhile;
+endif;
+  ?>
+</div>
+
+<?php get_footer(); ?>
